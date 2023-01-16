b1dbad185eb69a3625d130cf5d44e9a97812224f
“maydinca” <maydinca@gmail.com>
2023-01-15 21:19:56 -0500

    Dordun Katlari updated

--- src/EvenNumbers/DordunKatlari.java
+++ src/EvenNumbers/DordunKatlari.java
@@ -3,6 +3,11 @@ package EvenNumbers;
 import java.util.Scanner;
 
 public class DordunKatlari {
+/*
+Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve girilen değerlerden çift
+ ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+ */
+
     public static void main(String[] args) {
         int kullaniciSayisi, total = 0;
         Scanner scan = new Scanner(System.in);
