ddc6494d7fc915cfb105d37f8494d9daf2bbca7c
“maydinca” <maydinca@gmail.com>
2023-01-16 15:29:03 -0500

    Us Alma For Dongusu Hesabi yapan Program

---
+++ src/UsAlanProgramForDongusu.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class UsAlanProgramForDongusu {
+    /*
+    Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan programı "For Döngüsü" kullanarak yapınız.
+    */
+    public static void main(String[] args) {
+        int x, y, total = 1;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Lütfen üssü alınacak sayıyı giriniz = ");
+        x = input.nextInt();
+        System.out.print("Lütfen üs sayisini giriniz = ");
+        y = input.nextInt();
+
+        for (int i = 1; i <= y; i++) {
+            total *= x;
+        }
+        System.out.print("Ussu Alinmis Sayi ===> " + total);
+    }
+}
