76d80c1576a3e86eac10420e41d2fd38d1d25f98
maydinca <maydinca@gmail.com>
2023-01-31 23:17:34 -0500

    Revers Triangel

--- src/YildizlarIleUcgen/ReverseTriangle.java
+++ src/YildizlarIleUcgen/ReverseTriangle.java
@@ -6,7 +6,6 @@ public class ReverseTriangle {
         /** Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak, yıldızlar(*)
          * ile ekrana ters üçgen çizen programı yazın.
          */
-
         Scanner scan=new Scanner(System.in);
         System.out.print("How many rows do you want to make a triangle?:");
         int sayi=scan.nextInt();
@@ -16,11 +15,9 @@ public class ReverseTriangle {
             for (int k=sayi-i; k>0; k--){
                 System.out.print(" ");
             }
-
             for (int j=(2*i)-1; j>0; j--){
                 System.out.print("*");
             }
-
             System.out.println(" ");
         }
     }
