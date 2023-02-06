7a07586c86f74189c42d3db8a2ee6fc799e96977
maydinca <maydinca@gmail.com>
2023-02-06 18:32:29 -0500

    added tests

---
+++ src/test_01.java
@@ -0,0 +1,12 @@
+public class test_01 {
+    public static void main(String args[]) {
+        String x = "Patika.dev";
+        kodluyoruz(x);
+        System.out.println(x);
+    }
+    static void kodluyoruz(String y)
+    {
+        String x = "Java101";
+        y = "Kodluyoruz";
+    }
+}
