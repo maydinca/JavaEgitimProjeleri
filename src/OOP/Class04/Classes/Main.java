fa7fa539d84b889b03835b53e13047e56d80c42a
maydinca <maydinca@gmail.com>
2023-02-19 13:00:38 -0500

    OPP Class04 added

---
+++ src/OOP/Class04/Classes/Main.java
@@ -0,0 +1,21 @@
+package OOP.Class04.Classes;
+
+public class Main {
+    public static void main(String[] args) {
+        Car audi = new Car("Sports", "Audi", "red");
+        audi.increaseSpeed(20);
+        audi.printInfo();
+
+        Car bmw = new Car("Sports" , "BMW" , "blue");
+        bmw.increaseSpeed(10);
+        bmw.increaseSpeed(25);
+        bmw.increaseSpeed(5);
+        bmw.decreaseSpeed(25);
+        bmw.printInfo();
+
+        Car mercedes = new Car("Corporate" , "Mercedes" , "black");
+        mercedes.speed = 20;
+        mercedes.printInfo();
+
+    }
+}
